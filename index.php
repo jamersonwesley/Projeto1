31c2e240acf1e41da5ccb31dd4713ac490b815ff
jamersonwesley <jamerson_wesley@hotmail.com>
2021-09-15 11:12:50 -0300

    15/09/2021

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./css/fontawesome.css">
-     <link rel="stylesheet" href=".fontawesome.min.css">
      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
     <title>Document</title>
 </head>
 <body>
@@ -64,38 +63,43 @@
     </div>
     </section>
     <section class="descricao">
-    <div class="w50">     
+    <div class="w50 left">     
        <h2>Jamerson Wesley</h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, corporis maxime id sint delectus doloribus hic quia repellat itaque provident voluptas laboriosam culpa, beatae dicta voluptatem iure blanditiis pariatur minima!</p>
         </div>
-        <div class="w50"> 
-            <img src="./images/logo.jpg" alt="">
+        
+        <div class="w50 left"> 
+            <img src="./images/logo.jpg" width="300px" height="300px"alt="">
         </div>
+        <div class="clear"></div>
     </section>
     <section class="descricao">
         <h2 class="titulo">Descrição</h2>
     <div class="center">
-            <div class="especialidades"></div>
+            <div class="especialidades w33 left "></div>
             <h3><i class="fab fa-css3"></i></h3>
             <h3>CSS3</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, voluptatibus! Id odit animi eligendi, reiciendis numquam natus facilis nostrum laboriosam dolor minus cum necessitatibus ullam velit debitis quam? Rerum, in.</p>
         </div>
-        <div class="especialidades"></div>
+        <div class="clear"></div>
+        <div class="especialidades w33 left "></div>
             <h3><i class="fab fa-html5"></i></h3>
             <h3>HTML</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, voluptatibus! Id odit animi eligendi, reiciendis numquam natus facilis nostrum laboriosam dolor minus cum necessitatibus ullam velit debitis quam? Rerum, in.</p>
         </div>
-        <div class="especialidades"></div>
+        <div class="clear"></div>
+        <div class="especialidades w33 left"></div>
             <h3><i class="fab fa-php"></i></h3>
             <h3>PHP</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, voluptatibus! Id odit animi eligendi, reiciendis numquam natus facilis nostrum laboriosam dolor minus cum necessitatibus ullam velit debitis quam? Rerum, in.</p>
         </div>
+        <div class="clear"></div>
     </div>
 
     </section>
     <section class="extras">
         <div class="center">
-                <div class="w50">
+                <div class="w50 left">
                 <h2 class="titulo">Depoimentos</h2>
                 <div class="depoimento1">
                     <p class="depoimento-decricao">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil optio debitis repellat dolores doloremque ex magni distinctio enim provident molestiae eum fugit magnam facilis in, amet explicabo laudantium quas maxime?</p>
@@ -108,7 +112,8 @@
                 </div>
         </div>
      </div>
-         <div class="w50">
+     <div class="clear"></div>
+         <div class="w50 left">
                 <h2>Serviços</h2>
                 <div class="servicos">
                 <ul>
@@ -130,6 +135,7 @@
             <div class="center">
                 <p>Todos direitos reservados</p>
             </div>
+            <div class="clear"></div>
         </footer>
     
 </body>
